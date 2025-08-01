95afcc38cd6c01e34c3cfe30418a1eb798a46e07
Noel Mendoza <noelmendoza@Mac.attlocal.net>
2025-08-01 00:17:33 -0700

    added resilient route to point to local

--- server/src/app.ts
+++ server/src/app.ts
@@ -13,6 +13,7 @@ app.use(express.json());
 type HomeResponse = {
   message: string;
 };
+
 app.get<{}, HomeResponse>("/", (req, res) => {
   // utilize random number to generate between 0 and 1
   let randomNum = Math.random();
@@ -26,6 +27,24 @@ app.get<{}, HomeResponse>("/", (req, res) => {
     res.status (500).json({
       message: "500 Internal Server Error"
     })
+  }
+});
+
+
+
+app.get("/resilient-consumer", async (req, res) => {
+  const url = "http://localhost:3000/"; 
+
+  try {
+    const data = await fetchWithRetry(url, 3, 500);
+    res.status(200).json({ 
+      success: true, 
+      data });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: (err as Error).message,
+    });
   }
 });
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,6 @@ import app from "./app";
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`Server is listening on: http://localhost:${port}`);
   /* eslint-enable no-console */
 });
