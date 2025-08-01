ee1947e934136653b929e9f7d0e1f0d30e8beb97
Noel Mendoza <noelmendoza@Mac.attlocal.net>
2025-07-31 21:14:36 -0700

    cleaned up JSON body and client is now listening to server via fetch

--- client/src/app.ts
+++ client/src/app.ts
@@ -18,18 +18,24 @@ app.get<{}, HomeResponse>("/", async (req, res) => {
   let response = await fetch ("http://localhost:3000"); 
 
   // if response is 200 
-  if(response.status === 200) {
+  if (response.status === 200) {
+    // implement promise to get response body
     let body = await response.json();
-    res.json({
-      message: body,
+    res.json({ 
+      message: body.message 
     });
-  } 
-    // show user message
-  // otherwise
-    // show user error message 
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-  });
+  // if resonse is 500
+  } else if (response.status === 500) {
+    // implement promise to get response body
+    let body = await response.json();
+    res.status(500).json({
+      message: body.message 
+    });
+  }
+
+  // res.json({
+  //   message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
+  // });
 });
 
 export default app;
